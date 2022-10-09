35fb0cce9b134a543f930aff1f556cf0a677977f
Surajdhabale60 <112876188+Surajdhabale60@users.noreply.github.com>
2022-10-09 19:54:46 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,22 +1,11 @@
 ﻿namespace HelloWorld
 {
-    public class Program
+    class Program
     {
-        public static void Main(string[] args)
+        static void Main(string[] args)
         {
-            int a = 2;
-            int b = 3;
-
-            double d = 2.3;
-            bool value = true;
-            string str = "test";
-            char ch = 'r';
-
-            Console.WriteLine("The value of Integer is " + a);
-            Console.WriteLine("The value of double is " + d);
-            Console.WriteLine("The value of value is " + value);
-            Console.WriteLine("The value of string is " + str);
-            Console.WriteLine("The value of char is " + ch);
+            Console.WriteLine("Hello, Suraj");
+            Console.WriteLine("Welcome to Visual studio");
         }
     }
 }
