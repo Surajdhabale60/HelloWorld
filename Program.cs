7c08a11fe1529b609b88064d71817c385e4356d4
Surajdhabale60 <112876188+Surajdhabale60@users.noreply.github.com>
2022-10-09 19:53:05 +0530

    Main File To Call Function Of Particular Classes

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿namespace HelloWorld
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int a = 2;
+            int b = 3;
+
+            double d = 2.3;
+            bool value = true;
+            string str = "test";
+            char ch = 'r';
+
+            Console.WriteLine("The value of Integer is " + a);
+            Console.WriteLine("The value of double is " + d);
+            Console.WriteLine("The value of value is " + value);
+            Console.WriteLine("The value of string is " + str);
+            Console.WriteLine("The value of char is " + ch);
+        }
+    }
+}
